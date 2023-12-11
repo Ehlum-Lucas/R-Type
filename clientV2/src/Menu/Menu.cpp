4ac01dcff2a176938df5b9850dac03582b816fd6
Ariel <ariel.chambaz@epitech.eu>
2023-12-11 11:06:01 +0100

    (+) add: Menu

--- clientV2/src/Menu/Menu.cpp
+++ clientV2/src/Menu/Menu.cpp
@@ -7,7 +7,7 @@
 
 #include "Menu.hpp"
 
-Menu::Menu() : _window(sf::VideoMode::getFullscreenModes()[0], "SFML Window", sf::Style::Fullscreen)
+Menu::Menu() : _window(sf::VideoMode(1920, 1080), "SFML Window")
 {
     loadMenu();
 }
@@ -19,11 +19,85 @@ Menu::~Menu()
 void Menu::loopMenu()
 {
     while (_window.isOpen()) {
+
+        sf::Time deltaTime = _clock.restart();
+        
+        Update(deltaTime);
         Event();
-        _window.clear(sf::Color::Black);
-        _window.draw(_textbox);
-        _window.draw(_text);
-        _window.display();
+        Draw();
+    }
+}
+
+void Menu::Update(sf::Time deltaTime)
+{
+    float ratio = 762.5 / 1080;
+    float speedY = _diagonalSpeedBackground;
+    float speedX = _diagonalSpeedBackground * ratio;
+
+    _backgroundPosition.y += speedY * deltaTime.asSeconds();
+    _backgroundPosition.x += (- speedX * deltaTime.asSeconds());
+    _backgroundRect.setPosition(_backgroundPosition);
+
+    if (_backgroundPosition.y >= 0)
+    {
+        _backgroundPosition.y = -1080;
+        _backgroundPosition.x = 0;
+    }
+
+    float ratioStar =  1920.0 / 1080.0;
+    float starSpeedY = _diagonalSpeedStar;
+    float starSpeedX = _diagonalSpeedStar * ratioStar;
+
+    _starPosition.x += (-starSpeedX * deltaTime.asSeconds());
+    _starPosition.y += starSpeedY * deltaTime.asSeconds();
+    _starRect.setPosition(_starPosition);
+
+    if (_starPosition.y >= 0)
+    {
+        _starPosition.x = 0;
+        _starPosition.y = -1080;
+    }
+
+    float ratioPlanet =  1920.0 / 1080.0;
+    float planetSpeedY = _diagonalSpeedPlanet;
+    float planetSpeedX = _diagonalSpeedPlanet * ratioPlanet;
+
+    _planetPosition.x += (-planetSpeedX * deltaTime.asSeconds());
+    _planetPosition.y += planetSpeedY * deltaTime.asSeconds();
+    _planetRect.setPosition(_planetPosition);
+
+    if (_planetPosition.y >= 0)
+    {
+        _planetPosition.x = 0;
+        _planetPosition.y = -1080;
+    }
+
+    float ratioSaturn =  1920.0 / 1080.0;
+    float saturnSpeedY = _diagonalSpeedSaturn;
+    float saturnSpeedX = _diagonalSpeedSaturn * ratioSaturn;
+
+    _saturnPosition.x += (-saturnSpeedX * deltaTime.asSeconds());
+    _saturnPosition.y += saturnSpeedY * deltaTime.asSeconds();
+    _saturnRect.setPosition(_saturnPosition);
+
+    if (_saturnPosition.y >= 0)
+    {
+        _saturnPosition.x = 0;
+        _saturnPosition.y = -1080;
+    }
+
+    float ratioRocks =  1920.0 / 1080.0;
+    float rocksSpeedY = _diagonalSpeedRocks;
+    float rocksSpeedX = _diagonalSpeedRocks * ratioRocks;
+
+    _rocksPosition.x += (-rocksSpeedX * deltaTime.asSeconds());
+    _rocksPosition.y += rocksSpeedY * deltaTime.asSeconds();
+    _rocksRect.setPosition(_rocksPosition);
+
+    if (_rocksPosition.y >= 0)
+    {
+        _rocksPosition.x = 0;
+        _rocksPosition.y = -1080;
     }
 }
 
@@ -33,54 +107,211 @@ void Menu::Event()
     while (_window.pollEvent(event)) {
         if (event.type == sf::Event::Closed)
             _window.close();
-        else if (event.type == sf::Event::MouseButtonPressed) {
-            if (_textbox.getGlobalBounds().contains(_window.mapPixelToCoords(sf::Mouse::getPosition(_window)))) {
-                _isFocused = true;
-            } else {
-                _isFocused = false;
-            }
-        }
-        else if (event.type == sf::Event::TextEntered && _isFocused == true) {
-            if (event.text.unicode < 128 && event.text.unicode != 8) {
-                _input += static_cast<char>(event.text.unicode);
-                _text.setString(_input);
+        EventMouse(event);
+        EventTextEntered(event);
+        EventKey(event);
+    }
+}
 
-                if (_text.getLocalBounds().width > _textbox.getSize().x) {
-                    _text.move(-20, 0);
+void Menu::loadMenu()
+{
+    _font.loadFromFile("./assets/fonts/arcade_ya/ARCADE_R.ttf");
+
+    // Title Port
+    _titlePort.setFont(_font);
+    _titlePort.setCharacterSize(40);
+    _titlePort.setFillColor(sf::Color::White);
+    _titlePort.setString("Port:");
+    _titlePort.setPosition((_window.getSize().x / 2 - _titlePort.getGlobalBounds().width / 2) - 165, _window.getSize().y / 2 + 25);
+
+    // Box Input for Port Server
+    _textboxPort.setSize(sf::Vector2f(500, 50));
+    _textboxPort.setFillColor(sf::Color::White);
+    _textboxPort.setPosition(_window.getSize().x / 2 - _textboxPort.getSize().x / 2, (_window.getSize().y / 2) + 100);
+
+    // Text for Port Server
+    _textPort.setFont(_font);
+    _textPort.setCharacterSize(24);
+    _textPort.setFillColor(sf::Color::Black);
+
+    // Title Ip
+    _titleIp.setFont(_font);
+    _titleIp.setCharacterSize(40);
+    _titleIp.setFillColor(sf::Color::White);
+    _titleIp.setString("IP:");
+    _titleIp.setPosition((_window.getSize().x / 2 - _titleIp.getGlobalBounds().width / 2) - 210, _window.getSize().y / 2 - 125);
+
+    // BoxInput for Ip Server
+    _textboxIp.setSize(sf::Vector2f(500, 50));
+    _textboxIp.setFillColor(sf::Color::White);
+    _textboxIp.setPosition(_window.getSize().x / 2 - _textboxIp.getSize().x / 2, _window.getSize().y / 2 - 50);
+
+    // Text for Ip Server
+    _textIp.setFont(_font);
+    _textIp.setCharacterSize(24);
+    _textIp.setFillColor(sf::Color::Black);
+
+    // Title
+    _titleTexture.loadFromFile("./assets/images/title.png");
+    _titleSprite.setTexture(_titleTexture);
+    _titleSprite.setPosition(_window.getSize().x / 2 - _titleSprite.getGlobalBounds().width / 2, _window.getSize().y / 2 - 300);
+
+    // Play Button
+    _playButtonTexture.loadFromFile("./assets/images/play.png");
+    _playButtonSprite.setTexture(_playButtonTexture);
+    _playButtonHoverTexture.loadFromFile("./assets/images/playHover.png");
+    _playButtonSprite.setPosition(_window.getSize().x / 2 - _playButtonSprite.getGlobalBounds().width / 2, _window.getSize().y / 2 + 200);
+
+    // Background
+    _backgroundTexture.loadFromFile("./assets/images/background.png");
+    _backgroundPosition = sf::Vector2f(0, -1080);
+
+    _backgroundRect.setSize(sf::Vector2f(2683, 2160));
+    _backgroundRect.setTexture(&_backgroundTexture, false); 
+    _backgroundRect.setPosition(_backgroundPosition);
+
+    _diagonalSpeedBackground = 33.0f;
+
+    // Stars
+    _starTexture.loadFromFile("./assets/images/stars.png");
+    _starPosition = sf::Vector2f(0, -1080);
+
+    _starRect.setSize(sf::Vector2f(3840, 2160));
+    _starRect.setTexture(&_starTexture, false);
+
+    _diagonalSpeedStar = 42.0f;
+
+
+    // Planets
+    _planetTexture.loadFromFile("./assets/images/planet.png");
+    _planetPosition = sf::Vector2f(0, -1080);
+
+    _planetRect.setSize(sf::Vector2f(3840, 2160));
+    _planetRect.setTexture(&_planetTexture, false);
+
+    _diagonalSpeedPlanet = 55.0f;
+
+    // Saturns
+    _saturnTexture.loadFromFile("./assets/images/saturn.png");
+    _saturnPosition = sf::Vector2f(0, -1080);
+
+    _saturnRect.setSize(sf::Vector2f(3840, 2160));
+    _saturnRect.setTexture(&_saturnTexture, false);
+
+    _diagonalSpeedSaturn = 35.0f;
+
+    // Rocks
+    _rocksTexture.loadFromFile("./assets/images/rocks.png");
+    _rocksPosition = sf::Vector2f(0, -1080);
+
+    _rocksRect.setSize(sf::Vector2f(3840, 2160));
+    _rocksRect.setTexture(&_rocksTexture, false);
+
+    _diagonalSpeedRocks = 47.0f;
+
+    _isFocusedIp = false;
+    _isFocusedPort = false;
+}
+
+void Menu::loadAssets()
+{
+    // Ici on loadera tout les assets du jeu
+}
+
+void Menu::Draw()
+{
+    _window.clear(sf::Color::Black);
+    _window.draw(_backgroundRect);
+    _window.draw(_saturnRect);
+    _window.draw(_starRect);
+    _window.draw(_rocksRect);
+    _window.draw(_planetRect);
+    _window.draw(_textboxIp);
+    _window.draw(_textIp);
+    _window.draw(_textboxPort);
+    _window.draw(_textPort);
+    _window.draw(_titleSprite);
+    _window.draw(_playButtonSprite);
+    _window.draw(_titleIp);
+    _window.draw(_titlePort);
+    _window.display();
+}
+
+void Menu::EventKey(sf::Event event)
+{
+    if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Escape) {
+            _window.close();
+        }
+        else if (_isFocusedIp == true || _isFocusedPort == true) {
+            if (_inputIp.empty() && _isFocusedIp == true) {
+                _textIp.setPosition(_textboxIp.getPosition().x + 5, _textboxIp.getPosition().y + 5);
+            }
+            if (event.key.code == sf::Keyboard::BackSpace && !_inputIp.empty() && _isFocusedIp == true) {
+                _inputIp.pop_back();
+                _textIp.setString(_inputIp);
+                if (_textIp.getLocalBounds().width < _textboxIp.getSize().x) {
+                    _textIp.move(20, 0);
                 }
             }
-        }
-        else if (event.type == sf::Event::KeyPressed && _isFocused == true) {
-            if (_input.empty()) {
-                _text.setPosition(_textbox.getPosition().x + 5, _textbox.getPosition().y + 5);
+            if (_inputPort.empty() && _isFocusedPort == true) {
+                _textPort.setPosition(_textboxPort.getPosition().x + 5, _textboxPort.getPosition().y + 5);
             }
-            if (event.key.code == sf::Keyboard::BackSpace && !_input.empty()) {
-                _input.pop_back();
-                _text.setString(_input);
-                if (_text.getLocalBounds().width < _textbox.getSize().x) {
-                    _text.move(20, 0);
+            if (event.key.code == sf::Keyboard::BackSpace && !_inputPort.empty() && _isFocusedPort == true) {
+                _inputPort.pop_back();
+                _textPort.setString(_inputPort);
+                if (_textPort.getLocalBounds().width < _textboxPort.getSize().x) {
+                    _textPort.move(20, 0);
                 }
             }
         }
     }
 }
 
-void Menu::loadMenu()
+void Menu::EventTextEntered(sf::Event event)
 {
-    _textbox.setSize(sf::Vector2f(200, 100));
-    _textbox.setFillColor(sf::Color::White);
-    _textbox.setPosition(100, 100);
-
-    _font.loadFromFile("./assets/fonts/arcade_ya/ARCADE_R.ttf");
-    _text.setFont(_font);
-    _text.setCharacterSize(24);
-    _text.setFillColor(sf::Color::Black);
-    _text.setPosition(105, 105);
-
-    _isFocused = false;
+    if (event.type == sf::Event::TextEntered && (_isFocusedIp == true || _isFocusedPort == true)) {
+        if (event.text.unicode < 128 && event.text.unicode != 8 && _isFocusedIp == true) {
+            _inputIp += static_cast<char>(event.text.unicode);
+            _textIp.setString(_inputIp);
+            if (_textIp.getLocalBounds().width > _textboxIp.getSize().x) {
+                _textIp.move(-20, 0);
+            }
+        }
+        if (event.text.unicode < 128 && event.text.unicode != 8 && _isFocusedPort == true) {
+            _inputPort += static_cast<char>(event.text.unicode);
+            _textPort.setString(_inputPort);
+            if (_textPort.getLocalBounds().width > _textboxPort.getSize().x) {
+                _textPort.move(-20, 0);
+            }
+        }
+    }
 }
 
-void Menu::loadAssets()
+void Menu::EventMouse(sf::Event event)
 {
-    // Ici on loadera tout les assets du jeu
+    sf::Vector2f mousePos = _window.mapPixelToCoords(sf::Mouse::getPosition(_window));
+
+    if (_playButtonSprite.getGlobalBounds().contains(mousePos)) {
+        _playButtonSprite.setTexture(_playButtonHoverTexture);
+    } else {
+        _playButtonSprite.setTexture(_playButtonTexture);
+    }
+
+    if (event.type == sf::Event::MouseButtonPressed) {
+        if (_playButtonSprite.getGlobalBounds().contains(mousePos)) {
+            std::cout << "Ip: " << _inputIp << std::endl;
+            std::cout << "Port: " << _inputPort << std::endl;
+        }
+        if (_textboxIp.getGlobalBounds().contains(mousePos)) {
+            _isFocusedIp = true;
+            _isFocusedPort = false;
+        } else if (_textboxPort.getGlobalBounds().contains(mousePos)) {
+            _isFocusedPort = true;
+            _isFocusedIp = false;
+        } else {
+            _isFocusedIp = false;
+            _isFocusedPort = false;
+        }
+    }
 }
--- clientV2/src/Menu/Menu.hpp
+++ clientV2/src/Menu/Menu.hpp
@@ -9,6 +9,7 @@
     #define MENU_HPP_
     #include <SFML/Graphics.hpp>
     #include <memory> 
+    #include <iostream>
 
     class Menu {
         public:
@@ -18,15 +19,67 @@
             void loadAssets();
             void loadMenu();
             void Event();
+            void EventKey(sf::Event event);
+            void EventTextEntered(sf::Event event);
+            void EventMouse(sf::Event event);
+            void Draw();
+            void Update(sf::Time deltaTime);
+
+            // Ajouter la fonction pour gérer le clic sur le bouton "Play"
+            void handlePlayButton();
 
         protected:
         private:
             sf::RenderWindow _window;
-            sf::RectangleShape _textbox;
             sf::Font _font;
-            sf::Text _text;
-            std::string _input;
-            bool _isFocused;
+
+            sf::Text _titleIp;
+            sf::RectangleShape _textboxIp;
+            std::string _inputIp;
+            sf::Text _textIp;
+
+            sf::Text _titlePort;
+            sf::RectangleShape _textboxPort;
+            std::string _inputPort;
+            sf::Text _textPort;
+
+            sf::Texture _titleTexture;
+            sf::Sprite _titleSprite;
+            
+            sf::Texture _playButtonTexture;
+            sf::Sprite _playButtonSprite;
+            sf::Texture _playButtonHoverTexture;
+
+            sf::Texture _backgroundTexture;
+            sf::Vector2f _backgroundPosition;
+            sf::RectangleShape _backgroundRect;
+
+            sf::Texture _starTexture;
+            sf::Vector2f _starPosition;
+            sf::RectangleShape _starRect;
+
+            sf::Texture _planetTexture;
+            sf::Vector2f _planetPosition;
+            sf::RectangleShape _planetRect;
+
+            sf::Texture _saturnTexture;
+            sf::Vector2f _saturnPosition;
+            sf::RectangleShape _saturnRect;
+
+            sf::Texture _rocksTexture;
+            sf::Vector2f _rocksPosition;
+            sf::RectangleShape _rocksRect;
+            
+            sf::Clock _clock;
+
+            float _diagonalSpeedBackground;
+            float _diagonalSpeedStar;
+            float _diagonalSpeedPlanet;
+            float _diagonalSpeedSaturn;
+            float _diagonalSpeedRocks;
+            
+            bool _isFocusedIp;
+            bool _isFocusedPort;
     };
 
 #endif /* !MENU_HPP_ */
